d3d04ed00bbba8d95e9c73e89a5af80713086b6a
alifrayhan <alifrayhan92@gmail.com>
2024-11-08 10:47:42 +0600

    nav and banner push

---
+++ scripts.js
@@ -0,0 +1,6 @@
+let navlink = document.getElementById("navlink");
+let navicon = document.getElementById("navicon");
+
+navicon.addEventListener("click", () =>{
+    navlink.classList.toggle('hidden');
+})
